c38da49279e713664960e1c93f8c9562ff672a25
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2020-12-17 19:55:32 -0300

    feat: add styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #f5f5f5;
+    color: #333;
+  }
+
+  body, input, button {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 16px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
