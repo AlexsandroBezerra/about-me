2d9dcb55948785704ecb2fec6d27362b8f09a4e7
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2020-12-18 19:36:59 -0300

    feat: add head config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head'
+
 import GlobalStyle from '../styles/global'
 import { AppProps } from 'next/app'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <title>Alexsandro G Bezerra | Links</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+
       <GlobalStyle />
       <Component {...pageProps} />
     </>
